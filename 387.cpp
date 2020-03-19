4ae14416fdc77718086f7e9d9bcb47199d990b47
DeaDVPooL <43494522+DeaDVPooL@users.noreply.github.com>
2020-03-19 21:49:40 +0800

    Create 387.cpp

---
+++ 387.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> cnt;
+        for(int i=0;i<s.size();i++) cnt[s[i]]++;
+        for(int i=0;i<s.size();i++)
+            if(cnt[s[i]]==1)    return i;
+        return -1;
+    }
+};
