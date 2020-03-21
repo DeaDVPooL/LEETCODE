4caab960279ee8f2edaf873d813d1cd6bd3fa206
DeaDVPooL <43494522+DeaDVPooL@users.noreply.github.com>
2020-03-21 12:00:40 +0800

    Create 475.cpp

---
+++ 475.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+        int startindex = 0, maxn = 0;
+        for(int i = 0; i < houses.size(); i++) {
+            int tempmin = INT_MAX;
+            for(int j = startindex; j < heaters.size(); j++) {
+                if(abs(heaters[j] - houses[i]) <= tempmin) {
+                    tempmin = abs(heaters[j] - houses[i]);
+                    startindex = j;
+                } else {
+                    break;
+                }
+            }
+            maxn = max(maxn, tempmin);
+        }
+        return maxn;
+    }
+};
