a2942f126dff40db3bd1ef7a67abd41623bae2fd
DeaDVPooL <43494522+DeaDVPooL@users.noreply.github.com>
2020-03-24 09:15:33 +0800

    Create 538.cpp

---
+++ 538.cpp
@@ -0,0 +1,13 @@
+TreeNode* convertBST(TreeNode* root) {
+        dfs(root);
+        return root;
+    }
+private:
+    int sum = 0;
+    void dfs(TreeNode* root) {
+        if (root == NULL) return;
+        if (root->right != NULL) dfs(root->right);
+        sum += root->val;
+        root->val = sum;
+        if (root->left != NULL) dfs(root->left);
+    }
