b2e7f60832205c192eb44008747f927e6fda9db2
DeaDVPooL <43494522+DeaDVPooL@users.noreply.github.com>
2020-04-03 09:51:14 +0800

    Create 728.cpp

---
+++ 728.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+        for (int i = left; i <= right; i++) {
+            string s = to_string(i);
+            bool flag = true;
+            for (int j = 0; j < s.length(); j++) {
+                if (s[j] == '0' || i % (s[j] - '0') != 0) {
+                    flag = false;
+                    break;
+                }
+            }  
+            if (flag) ans.push_back(i);
+        }
+        return ans;
+    }
+};
