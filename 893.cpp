88091108538cb8d2a008879849ed150ec2ac01fb
DeaDVPooL <43494522+DeaDVPooL@users.noreply.github.com>
2020-04-10 09:53:26 +0800

    Create 893.cpp

---
+++ 893.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSpecialEquivGroups(vector<string>& A) {
+        unordered_set<string> Set;
+        for (const auto &str : A){
+            string even, odd;
+            for( int i=0; i<str.size(); i++){
+                if (i % 2)
+                    odd += str[i];
+                else
+                    even += str[i];
+            }
+            sort(even.begin(), even.end());
+            sort(odd.begin(), odd.end());
+            Set.insert(odd+even);
+        }
+        return Set.size();
+    }
+};
