8addc38be6fce9110be2c6657bf7c82671fded06
DeaDVPooL <43494522+DeaDVPooL@users.noreply.github.com>
2020-04-21 10:17:00 +0800

    Create 1046.cpp

---
+++ 1046.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int,vector<int>,less<int> > q;
+        for(int i=0;i<stones.size();i++)    q.push(stones[i]);
+        while(q.size()>1)
+        {
+            int tmp1 = q.top();
+            q.pop();
+            int tmp2 = q.top();
+            q.pop();
+            q.push(tmp1-tmp2);
+        }
+        return q.size()==0?0:q.top();
+    }
+};
