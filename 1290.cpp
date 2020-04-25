361df07a41edb4abfe7f773ee5c91ed70064038a
DeaDVPooL <43494522+DeaDVPooL@users.noreply.github.com>
2020-04-25 18:28:02 +0800

    Create 1290.cpp

---
+++ 1290.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        ListNode* cur = head;
+        int ans = 0;
+        while (cur != nullptr) {
+            ans = ans * 2 + cur->val;
+            cur = cur->next;
+        }
+        return ans;
+    }
+};
