be8bbffd0bf5f21987f8b1c541a597e227d21aaf
DeaDVPooL <43494522+DeaDVPooL@users.noreply.github.com>
2020-04-27 11:17:34 +0800

    Create 1304.cpp

---
+++ 1304.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ans;
+        if(n%2==1)  ans.emplace_back(0);
+        for(int i=1;i<=n/2;i++)
+        {
+            ans.emplace_back(i);
+            ans.emplace_back(-i);
+        }
+        return ans;
+    }
+};
