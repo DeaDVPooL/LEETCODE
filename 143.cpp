b3fd87911a0771e2712811daa3ca48d38e143199
DeaDVPooL <43494522+DeaDVPooL@users.noreply.github.com>
2020-07-02 22:39:45 +0800

    Create 143.cpp

---
+++ 143.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* p=head,*q=head,*r,*s=head;
+        if(!head)            //head为空，则直接退出
+            return ;         
+        while(q->next){      //寻找中间结点
+            q=q->next;       //p走一步
+            p=p->next;
+            if(q->next)
+              q=q->next;     //q走两步
+        }
+        q=p->next;           //p所指结点为中间结点，q为后半段链表的首结点
+        p->next=nullptr;
+        while(q){            //将链表后半段逆置
+            r=q->next;
+            q->next=p->next;
+            p->next=q;
+            q=r;
+        }
+        q=p->next;            //q指向后半段的第一个数据结点
+        p->next=nullptr;
+        while(q){             //将链表后半段的结点插入到指定位置
+            r=q->next;        //r指向后半段的下一个结点
+            q->next=s->next;  //将q所指结点插入到s所指结点（head结点）之后
+            s->next=q;        
+            s=q->next;        //s指向前半段的下一个插入点
+            q=r;
+        }
+    }
+};
